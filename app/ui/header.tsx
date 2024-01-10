3404b1aa08734a958436cf6bb97a766f89c2c1a8
Sethuraman Srinivasan <ramseth@kyperx.com>
2024-01-10 11:41:10 +1100

    header and footer created

---
+++ app/ui/header.tsx
@@ -0,0 +1,26 @@
+import Logo from "@/public/images/logo.svg"
+import Link from 'next/link'
+import Image from "next/image"
+import { Montserrat } from 'next/font/google'
+
+const lato = Montserrat({
+    subsets: ['latin'],
+    weight: ["300", "400", "500", "600",],
+    style: ["normal"]
+})
+
+export default function Header() {
+    return (
+        <div className="container mx-auto">
+            <header className={`flex justify-between items-center p-8 ${lato.className}`}>
+                <Link href="/" className="flex items-center w-[105px] h-[36px] sm:w-[140px] sm:h-[48px]">
+                    <Image className="inline" src={Logo} alt="Splitwise Logo" />
+                </Link>
+                <div className="text-xs sm:text-sm md:text-normal flex items-center">
+                    <Link href="/login" className="px-3 py-2 sm:px-5 sm:py-3 text-teal rounded font-semibold button-hover">Log in</Link>
+                    <Link href="/signup" className="bg-teal px-3 py-2 sm:px-5 sm:py-3 text-white rounded shadow font-semibold button-hover">Sign up</Link>
+                </div>
+            </header>
+        </div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        transparent: 'transparent',
+        'teal': '#1cc29f',
+        'grey-light': '#52595f'
+      }
     },
   },
   plugins: [],
